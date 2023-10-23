9fc6f5e50b46ed990e290d7f1918fc0148414d6d
Yip Tuck Wai <yiptckwai@gmail.com>
2023-10-23 17:37:26 +0800

    Update forms.py

--- project/bug/forms.py
+++ project/bug/forms.py
@@ -7,17 +7,8 @@ class BugRegistrationForm(forms.ModelForm):
         fields = ['description', 'bug_type', 'report_date', 'status']
 
         widgets = {
-            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control','placeholder':'Text description for the bug'}),
             'bug_type': forms.Select(attrs={'class': 'form-control'}),
             'report_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'status': forms.Select(attrs={'class': 'form-control'}),
         } 
-    
-#def clean(self):
-
-#        super(BugRegistrationForm, self).clean()
- #       description = self.cleaned_data.get('description')
-#        if len(description) == 0 :
- #           self.errors['description'] = self.error_class(['This field is required'])
-#
- #       return self.cleaned_data
