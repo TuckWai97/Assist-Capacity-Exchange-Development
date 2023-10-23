7c4fb4c5d1be24bd08a8b2798f74c134cac27fd2
Yip Tuck Wai <yiptckwai@gmail.com>
2023-10-23 12:44:35 +0800

    Update settings.py

--- project/Bugapp/settings.py
+++ project/Bugapp/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "crispy_forms",
+    "crispy_bootstrap5",
     "bug.apps.BugConfig",
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
