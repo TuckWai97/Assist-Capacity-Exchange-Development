e8c0687d7ef597e810eb375656dd20e067172d0f
Yip Tuck Wai <yiptckwai@gmail.com>
2023-10-23 14:26:19 +0800

    Update urls.py

--- project/bug/urls.py
+++ project/bug/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "bug"
 urlpatterns = [
     path('register/', views.register_bug, name='register_bug'),
     path('<int:bug_id>/', views.view_bug, name='view_bug'),
