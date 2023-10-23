779701206803230ec335b0b1e541e8aab3ae5a87
Yip Tuck Wai <yiptckwai@gmail.com>
2023-10-23 14:38:53 +0800

    Update views.py

--- project/bug/views.py
+++ project/bug/views.py
@@ -18,12 +18,12 @@ def register_bug(request):
             # You can add a success message
             #messages.success(request,  'Bug is registered successfully!')
             # redirect back to bug detail page after successful registering a bug             
-            return redirect('view_bug', bug.id)
+            return redirect('bug:view_bug', bug.id)
     else:
         # If it is not POST request, display the registration form
         form = BugRegistrationForm()
 
-    return render(request, 'bug_register.html', {'form': form})
+    return render(request, 'bug/bug_register.html', {'form': form})
 
 # function with a list of fields of the bug
 def view_bug(request, bug_id):
@@ -31,9 +31,9 @@ def view_bug(request, bug_id):
     # Retrieve specific bug using its ID or handle 404 error if it does not exist
     #pk means as primary key as variable to store bug's ID
     bug = get_object_or_404(Bug, pk=bug_id)
-    return render(request, 'view_bug.html', {'bug':bug})
+    return render(request, 'bug/view_bug.html', {'bug':bug})
 
 def list_bug(request):
     # Retrieve all bugs from the database
     bugs = Bug.objects.all()
-    return render(request, 'bug_list.html', {'bugs':bugs})
+    return render(request, 'bug/bug_list.html', {'bugs':bugs})
