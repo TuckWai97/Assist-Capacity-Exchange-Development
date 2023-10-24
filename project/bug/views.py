d57e4d5360ddd4e142a4ef5c5695de5ca1f77d93
Yip Tuck Wai <yiptckwai@gmail.com>
2023-10-24 20:07:06 +0800

    Update views.py

--- project/bug/views.py
+++ project/bug/views.py
@@ -8,28 +8,44 @@ from django.urls import reverse
 
 # Define a view for creating a new Bug
 class BugCreateView(CreateView):
-    model = Bug  # Operates on the Bug model
-    form_class = BugRegistrationForm  # Use the BugRegistrationForm
-    template_name = 'bug/bug_register.html'  # Render this form template
 
-    def get_success_url(self):   
+    # Operates on the Bug model
+    model = Bug  
+
+    # Use the BugRegistrationForm
+    form_class = BugRegistrationForm 
+
+    # Render this form template
+    template_name = 'bug/bug_register.html'  
+
+    def get_success_url(self):
+
         # Reverse is a Django utility for building URLs. It takes a URL pattern name
         # and any arguments needed to create the URL.
         # In this case, it generates the URL for the detail view of the 'Bug' model
         # using the URL pattern name 'bug:view_bug'. We pass the bug's ID as an argument
         # to ensure that the URL is unique for each bug.
         success_url = reverse('bug:view_bug', kwargs={"pk": self.object.pk})
+
         # Add this line for debugging
         #print("Success URL:", success_url)  
         return success_url
 
 # Define a view for displaying Bug details
 class BugDetailView(DetailView):
-    model = Bug  # Operates on the Bug model
-    template_name = 'bug/view_bug.html'  # Render this detail template
+
+    # Operates on the Bug model
+    model = Bug
+
+    # Render this detail template
+    template_name = 'bug/view_bug.html'  
 
 # Define a view for listing all Bugs
 class BugListView(ListView):
-    model = Bug  # Operates on the Bug model
-    template_name = 'bug/bug_list.html'  # Render this list template
+
+    # Operates on the Bug model
+    model = Bug
+
+    # Render this list template  
+    template_name = 'bug/bug_list.html'  
     context_object_name = 'bug_list'
