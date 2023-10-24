724d6dbda2f8383910b273e14d1127e45d133349
Yip Tuck Wai <yiptckwai@gmail.com>
2023-10-25 00:44:09 +0800

    Update urls.py

--- project/bug/urls.py
+++ project/bug/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = "bug"
 urlpatterns = [
+    path('', views.home, name='home'),
     path('register/', views.BugCreateView.as_view(), name='register_bug'),
     path('<int:pk>/', views.BugDetailView.as_view(), name='view_bug'),
     path('list/', views.BugListView.as_view(), name='list_bug'),
